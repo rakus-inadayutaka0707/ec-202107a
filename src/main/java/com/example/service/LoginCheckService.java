4eac2b656e0224fe3be81df7b8f673701155b8ac
rakus-inadayutaka0707 <yutaka.inada@rakus-partners.co.jp>
2021-09-03 12:54:58 +0900

    Javadocコメントを追加しました

--- src/main/java/com/example/service/LoginCheckService.java
+++ src/main/java/com/example/service/LoginCheckService.java
@@ -5,14 +5,26 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * ログイン確認を行い際の業務処理をするServiceクラス.
+ * 
+ * @author inada
+ *
+ */
 @Service
 public class LoginCheckService {
 
 	@Autowired
 	private HttpSession session;
-	
+
+	/**
+	 * セッションスコープにuserが格納されていなければログイン画面に遷移する.
+	 * 
+	 * @param url ログイン後に戻ってくるURL
+	 * @return 真偽値
+	 */
 	public Boolean loginCheck(String url) {
-		if(session.getAttribute("user") == null) {
+		if (session.getAttribute("user") == null) {
 			session.setAttribute("url", url);
 			return true;
 		}
