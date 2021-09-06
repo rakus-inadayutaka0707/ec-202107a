455bc73d9dec905602c49ccce591a15e1787ce6b
rakus-inadayutaka0707 <yutaka.inada@rakus-partners.co.jp>
2021-09-06 21:41:35 +0900

    表示の不備を修正しました

--- src/main/java/com/example/form/DecisionOrderForm.java
+++ src/main/java/com/example/form/DecisionOrderForm.java
@@ -27,6 +27,7 @@ public class DecisionOrderForm {
 
 	/** 宛先Eメール */
 	@Email(message = "メールアドレスの形式ではありません")
+	@NotBlank(message = "メールアドレスを入力してください")
 	private String destinationEmail;
 
 	/** 宛先郵便番号 */
